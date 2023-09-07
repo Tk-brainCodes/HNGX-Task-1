a36bab5c58dd89c07dd58ef6510ec53eae3f4942
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-09-07 09:55:07 +0100

    feat: added basic slack information, created assests, styles, page, and script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function updateDataAttributes () {
+  const newDate = new Date()
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
+  const currentDay = daysOfWeek[newDate.getUTCDay()]
+  const currentUTC = newDate.toUTCString()
+
+  document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  ).textContent = `Current Day: ${currentDay}`
+  document.querySelector(
+    '[data-testid="currentUTCTime"]'
+  ).textContent = `Current UTC Time: ${currentUTC}`
+}
+
+window.onload = updateDataAttributes
